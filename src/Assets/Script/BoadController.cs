d27115113da6fef282649e1e3e4a65f036627360
Mocha0924 <108125975+Mocha0924@users.noreply.github.com>
2023-05-18 11:33:32 +0900

    第５回講義内容

--- src/Assets/Script/BoadController.cs
+++ src/Assets/Script/BoadController.cs
@@ -25,13 +25,7 @@ public class BoadController : MonoBehaviour
     public void Start()
     {
         ClearAll();
-        for (int y = 0; y < BOARD_HEIGHT; y++)
-        {
-            for (int x = 0; x < BOARD_WIDTH; x++)
-            {
-                Settle(new Vector2Int(x, y), Random.Range(1, 7));
-            }
-        }
+       
     }
     public static bool IsValidated(Vector2Int pos)
     {
